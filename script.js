7899e9737d4bb08405bf990eee51e5c23dc29326
ykmsd <yuka1110m@gmail.com>
2024-04-24 19:47:41 +0200

    disable

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-console.log('Hello!');
-
 async function test() {
     try {
-      const response = await fetch("https://google.com");
+      const response = await fetch("https://echo.zuplo.io/foo?bar=1");
       const test = await response.json();
       console.log('test', test);
     } catch {
       console.log('error');
     }
+}
 
-  }
-
-  test();
+test();
