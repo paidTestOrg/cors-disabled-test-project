f07bb400f051e58c7295c1b757bfbbcf8c3b568e
ykmsd <yuka1110m@gmail.com>
2024-04-24 23:09:37 +0200

    cleanup

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 async function test() {
-    try {
-      const response = await fetch("https://echo.zuplo.io/foo?bar=1");
-      const test = await response.json();
-      console.log('test', test);
-    } catch {
-      console.log('error');
-    }
 }
 
 test();
